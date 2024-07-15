b55578de5ec75eef30d9d1e7537b75625e363344
Joao Victor <ovictorjo3@gmail.com>
2024-07-15 10:17:52 -0400

    construindo função menu

---
+++ main.py
@@ -0,0 +1,12 @@
+import jogo as j
+import fileHandler as fH
+
+def mostrar_menu():
+    print("="*30)
+    print(" "*7 + "Jogo da Forca")
+    print("="*30)
+    print("\n1 - JOGAR")
+    print("2 - SCORE")
+    print("3 - SAIR\n")
+    print(""*30)
+
